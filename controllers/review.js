62dde78844cb5d72630a9e129ad4442aa23ccb91
aleksandershurdho20 <aleksandershurdho@gmail.com>
2021-04-11 22:22:56 +0200

    review controllers

--- controllers/review.js
+++ controllers/review.js
@@ -1,15 +1,40 @@
 const Review = require('../models/Reviews')
 
 exports.createReview = async (req, res) => {
-    const { reviewedBy, review, comment, ratting, product } = req.body;
+    const { reviewedBy, comment, review, ratting, product } = req.body;
     try {
-        const review = new Review({
+        const createdReview = new Review({
             reviewedBy, review, comment, ratting, product
         })
-        await review.save();
+        await createdReview.save();
         res.status(200).json({
-            review
+            createdReview
         })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error })
+    }
+}
+
+exports.getAllReviews = async (req, res) => {
+    try {
+        const reviews = await Review.find({})
+        if (reviews.length > 0) {
+            res.json({ reviews })
+
+        }
+        else {
+            res.status(404).json({ errorr: 'Reviews not found!' })
+        }
+    } catch (error) {
+        res.status(500).json({ error })
+    }
+}
+exports.getReview = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const review = await Review.findById(id)
+        res.json({ review })
     } catch (error) {
         res.status(500).json({ error })
     }
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,5 +3,6 @@ const Reviews = require('../controllers/review')
 const router = express.Router();
 
 router.post('/review/create', Reviews.createReview)
-
+router.get('/reviews', Reviews.getAllReviews)
+router.get('/review/:id', Reviews.getReview)
 module.exports = router
