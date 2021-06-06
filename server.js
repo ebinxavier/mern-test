f1b36c7549589e98b0943939742e763cee44e772
aleksander shurdho <aleksandershurdho@gmail.com>
2021-06-06 21:27:45 +0200

    added setup for deploy

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const product = require('./routes/product')
 const users = require('./routes/users')
 const orders = require('./routes/orders')
 const reviews = require('./routes/reviews')
+const path = require("path");
 dotenv.config()
 //middlewares
 app.use(cors())
@@ -26,4 +27,11 @@ app.use('/uploads', express.static('uploads'))
 
 connectDb();
 const port = process.env.PORT || 5000
+
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 app.listen(port, () => console.log(`listen port ${port}`))
